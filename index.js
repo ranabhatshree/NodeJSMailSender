34c34476818449f98a753c2b241e1c9d017bc83b
unknown <ranabhatshree111@gmail.com>
2021-03-31 12:25:45 +0545

    added email validator

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const validator = require("email-validator");
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -14,7 +15,21 @@ app.use(bodyParser.json())
   })
 
   app.post('/send-mail', function (req, res) {
-    res.status(200).send('ok')
+    if(req.body.email && req.body.message) {
+      const email =  req.body.email
+      if(validator.validate(email)) {
+        res.status(200).send({
+          'message': 'Everything is ok!'
+        })
+      }else{
+        res.status(422).send({
+              messae: "Invalid E-mail Address!!",
+          })
+      }
+    }
+    res.status(422).send({
+          messae: "E-mail and Message Required!!",
+        })
   })
 
   function sendMail(website, status) {
