2810ec2092cbd1ed4bd92b8d3c52f0444bb146e4
unknown <ranabhatshree111@gmail.com>
2021-03-31 13:07:13 +0545

    wrap up!!!

--- index.js
+++ index.js
@@ -2,63 +2,68 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
-const validator = require("email-validator");
+const port = process.env.PORT || 3000
+const validator = require("email-validator")
+const nodemailer = require('nodemailer')
+require('dotenv').config()
 
 app.use(cors())
 app.use(bodyParser.json())
 
   app.get('/', function (req, res) {
     res.status(200).send({
-      'message': 'Everything is ok!'
+      'message': "OK!!"
     })
   })
 
   app.post('/send-mail', function (req, res) {
     if(req.body.email && req.body.message) {
-      const email =  req.body.email
+      var email =  req.body.email
+      var message = req.body.message
+      var subject = req.body.subject ? req.body.subject : "Alert!!!"
+
       if(validator.validate(email)) {
+        sendMail(to=email, subject=subject, message=message)
         res.status(200).send({
-          'message': 'Everything is ok!'
+          message: "Mail sent success!!!"
         })
       }else{
         res.status(422).send({
-              messae: "Invalid E-mail Address!!",
+              message: "Invalid E-mail Address!",
           })
       }
+    }else{
+      res.status(422).send({
+            message: "E-mail and Message Required !",
+          })
     }
-    res.status(422).send({
-          messae: "E-mail and Message Required!!",
-        })
   })
 
-  function sendMail(website, status) {
+  function sendMail(to, subject, message) {
     let transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        type: 'OAuth2',
-        user: process.env.MAIL_USERNAME,
-        pass: process.env.MAIL_PASSWORD,
-        clientId: process.env.OAUTH_CLIENTID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+        type: 'login',
+        user: process.env.MAIL_AUTH_USER,
+        pass: process.env.MAIL_AUTH_PASSWORD,
       }
     });
 
     let mailOptions = {
-      from: process.env.MAIL_USERNAME,
-      to: process.env.MAIL_USERNAME,
-      subject: 'ProjectChecker - Website Is Down',
-      text: 'The website ' + website + ' returned with a status of ' + status
+      from: process.env.MAIL_AUTH_USER,
+      to: to,
+      subject: subject,
+      text: message
     };
 
     transporter.sendMail(mailOptions, function(err, data) {
       if (err) {
-        console.log("Error " + err);
-      } else {
-
+        console.log(err)
       }
-    });
+      else{
+        console.log(data)
+      }
+    })
 
   }
 
